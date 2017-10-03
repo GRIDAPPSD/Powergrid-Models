2586cc252beb9da03ea0493ac1284b7085e0be71
temcdrm <Thomas.McDermott@pnnl.gov>
2017-10-02 23:31:44 -0700

    using Maven on Windows

--- blazegraph/import.bat
+++ blazegraph/import.bat
@@ -1,39 +1,11 @@
 set JENA_HOME=c:\apache-jena-3.1.0
-set CLASSPATH=.;c:/apache-jena-3.1.0/lib/*;c:/commons-math3-3.6.1/*
+set CLASSPATH=target/*;c:/apache-jena-3.1.0/lib/*;c:/commons-math3-3.6.1/*
 
-rem javac GldNode.java
+rem mvn clean install
 
-rem javac DistBaseVoltage.java
-rem javac DistCapacitor.java
-rem javac DistComponent.java
-rem javac DistConcentricNeutralCable.java
-rem javac DistCoordinates.java
-rem javac DistLinesCodeZ.java
-rem javac DistLinesInstanceZ.java
-rem javac DistLineSegment.java
-rem javac DistLineSpacing.java
-rem javac DistLinesSpacingZ.java
-rem javac DistLoad.java
-rem javac DistOverheadWire.java
-rem javac DistPhaseMatrix.java
-rem javac DistPowerXfmrCore.java
-rem javac DistPowerXfmrMesh.java
-rem javac DistPowerXfmrWinding.java
-rem javac DistRegulator.java
-rem javac DistSequenceMatrix.java
-rem javac DistSubstation.java
-rem javac DistSwitch.java
-rem javac DistTapeShieldCable.java
-rem javac DistXfmrCodeOCTest.java
-rem javac DistXfmrCodeRating.java
-rem javac DistXfmrCodeSCTest.java
-rem javac DistXfmrBank.java
-rem javac DistXfmrTank.java
-
-javac CIMImporter.java
-
-rem java CIMImporter ieee13
-rem java CIMImporter ieee13_assets
-java CIMImporter -l=1.0 -i=1 -n=zipload_schedule ieee8500
+rem java gov.pnnl.goss.cim2glm.CIMImporter ieee13
+rem java gov.pnnl.goss.cim2glm.CIMImporter ieee13_assets
+java gov.pnnl.goss.cim2glm.CIMImporter -o=glm -l=1.0 -i=1 ieee8500
+java gov.pnnl.goss.cim2glm.CIMImporter -o=dss -l=1.0 -i=1 ieee8500
 
 
