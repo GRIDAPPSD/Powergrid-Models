2da4eaf88dcfdbf3c82801fa2e852cf8ab14d7e3
temcdrm <Thomas.McDermott@pnnl.gov>
2018-02-19 17:20:35 -0800

    working on select by feeder

--- blazegraph/import.bat
+++ blazegraph/import.bat
@@ -3,6 +3,7 @@ set CLASSPATH=target/*;c:/apache-jena-3.6.0/lib/*;c:/commons-math3-3.6.1/*
 
 rem mvn clean install
 
-java gov.pnnl.goss.cim2glm.CIMImporter -o=dss -l=1.0 -i=1 test
+rem java gov.pnnl.goss.cim2glm.CIMImporter -o=idx test
+java gov.pnnl.goss.cim2glm.CIMImporter -s=_C1C3E687-6FFD-C753-582B-632A27E28507 -o=dss -l=1.0 -i=1 test
 
 
