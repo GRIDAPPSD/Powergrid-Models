4e13e635f20994672af7454d52514d641387754d
temcdrm <Thomas.McDermott@pnnl.gov>
2018-02-24 12:06:31 -0800

    switch to using SparqlWrapper2

--- blazegraph/ListFeeders.py
+++ blazegraph/ListFeeders.py
@@ -1,6 +1,7 @@
-from SPARQLWrapper import SPARQLWrapper, JSON
+from SPARQLWrapper import SPARQLWrapper, SPARQLWrapper2, JSON
 
-sparql = SPARQLWrapper("http://localhost:9999/blazegraph/namespace/kb/sparql")
+#sparql = SPARQLWrapper("http://localhost:9999/blazegraph/namespace/kb/sparql")
+sparql = SPARQLWrapper2("http://localhost:9999/blazegraph/namespace/kb/sparql")
 
 sparql.setQuery("""
     PREFIX r: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
@@ -23,14 +24,18 @@ sparql.setQuery("""
     ORDER by ?station ?feeder
 """)
 
-sparql.setReturnFormat(JSON)
-results = sparql.query().convert()
+ret = sparql.query()
+print ('binding keys are:',ret.variables)
+#print (ret.bindings)
+for b in ret.bindings:
+	print (b['feeder'].value,b['fid'].value)
 
+#sparql.setReturnFormat(JSON)
+#results = sparql.query().convert()
 #print (results)
-
-for result in results["results"]["bindings"]:
-    print(result["feeder"]["value"],result["fid"]["value"],result["station"]["value"],
-          result["subregion"]["value"],result["region"]["value"])
+#for result in results["results"]["bindings"]:
+#    print(result["feeder"]["value"],result["fid"]["value"],result["station"]["value"],
+#          result["subregion"]["value"],result["region"]["value"])
 
 
    
