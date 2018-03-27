785d1102c4e87ce16991584b9ae03730932682cc
Gibson, Tara D <tara@pnnl.gov>
2018-03-27 12:00:06 -0700

    Updated packaging and sample run scripts to make it easier to build and run from command line

--- blazegraph/import.bat
+++ blazegraph/import.bat
@@ -1,9 +1,7 @@
-set JENA_HOME=c:\apache-jena-3.6.0
-set CLASSPATH=target/*;c:/apache-jena-3.6.0/lib/*;c:/commons-math3-3.6.1/*
 
-rem mvn clean install
+mvn clean install
 
 rem java gov.pnnl.goss.cim2glm.CIMImporter -o=idx test
-java gov.pnnl.goss.cim2glm.CIMImporter -s=_C1C3E687-6FFD-C753-582B-632A27E28507 -o=dss -l=1.0 -i=1 test
+java -jar -jar  target\cim2glm-0.0.1-SNAPSHOT.jar -u=http://localhost:9999/blazegraph/namespace/kb/sparql -s=_4F76A5F9-271D-9EB8-5E31-AA362D86F2C3 -o=glm -l=1.0 -i=1 test
 
 
