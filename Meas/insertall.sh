3105953398f2ab3429efa90117a7e47d5cc36077
temcdrm <Thomas.McDermott@pnnl.gov>
2018-04-11 08:59:55 -0700

    batch listing and inserting for measurements

---
+++ Meas/insertall.sh
@@ -0,0 +1,19 @@
+# python InsertMeasurements.py ieee8500_rc1.bak
+
+python InsertMeasurements.py ieee123_special.txt
+python InsertMeasurements.py ieee123_switch_i.txt
+python InsertMeasurements.py ieee123_node_v.txt
+
+python InsertMeasurements.py ieee8500_special.txt
+python InsertMeasurements.py ieee8500_switch_i.txt
+python InsertMeasurements.py ieee8500_node_v.txt
+
+python InsertMeasurements.py eprij1_special.txt
+python InsertMeasurements.py eprij1_switch_i.txt
+python InsertMeasurements.py eprij1_node_v.txt
+
+python InsertMeasurements.py r2_12_47_2_special.txt
+python InsertMeasurements.py r2_12_47_2_switch_i.txt
+python InsertMeasurements.py r2_12_47_2_node_v.txt
+
+
---
+++ Meas/listall.sh
@@ -0,0 +1,5 @@
+python ListMeasureables.py ieee123 _C1C3E687-6FFD-C753-582B-632A27E28507
+python ListMeasureables.py ieee8500 _4F76A5F9-271D-9EB8-5E31-AA362D86F2C3
+python ListMeasureables.py eprij1 _67AB291F-DCCD-31B7-B499-338206B9828F
+python ListMeasureables.py r2_12_47_2 _9CE150A8-8CC5-A0F9-B67E-BBD8C79D3095
+
