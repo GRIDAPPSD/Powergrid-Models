5c760b3f9192895bb086a2a8147cfb392162ab4d
temcdrm <Thomas.McDermott@pnnl.gov>
2018-06-23 13:24:53 -0600

    measurements for Breakers and Reclosers

--- Meas/insertall.sh
+++ Meas/insertall.sh
@@ -1,31 +1,33 @@
 # python InsertMeasurements.py ieee8500_rc1.bak
 
-python InsertMeasurements.py ieee123_special.txt
+python InsertMeasurements.py ieee13nodeckt_switch_i.txt
+
+#python InsertMeasurements.py ieee123_special.txt
 #python InsertMeasurements.py ieee123_switch_i.txt
-python InsertMeasurements.py ieee123_node_v.txt
-python InsertMeasurements.py ieee123_supplement.txt
+#python InsertMeasurements.py ieee123_node_v.txt
+#python InsertMeasurements.py ieee123_supplement.txt
 #python InsertMeasurements.py ieee123_loads.txt
 #python InsertMeasurements.py ieee123_lines_pq.txt
 #python InsertMeasurements.py ieee123_xfmr_pq.txt
 
-python InsertMeasurements.py ieee8500_special.txt
-python InsertMeasurements.py ieee8500_switch_i.txt
-python InsertMeasurements.py ieee8500_node_v.txt
-python InsertMeasurements.py ieee8500_rc1_supplement.txt
+#python InsertMeasurements.py ieee8500_special.txt
+#python InsertMeasurements.py ieee8500_switch_i.txt
+#python InsertMeasurements.py ieee8500_node_v.txt
+#python InsertMeasurements.py ieee8500_rc1_supplement.txt
 #python InsertMeasurements.py ieee8500_loads.txt
 #python InsertMeasurements.py ieee8500_lines_pq.txt
 #python InsertMeasurements.py ieee8500_xfmr_pq.txt
 
-python InsertMeasurements.py eprij1_special.txt
-python InsertMeasurements.py eprij1_switch_i.txt
-python InsertMeasurements.py eprij1_node_v.txt
+#python InsertMeasurements.py eprij1_special.txt
+#python InsertMeasurements.py eprij1_switch_i.txt
+#python InsertMeasurements.py eprij1_node_v.txt
 #python InsertMeasurements.py eprij1_loads.txt
 #python InsertMeasurements.py eprij1_lines_pq.txt
 #python InsertMeasurements.py eprij1_xfmr_pq.txt
 
-python InsertMeasurements.py r2_12_47_2_special.txt
-python InsertMeasurements.py r2_12_47_2_switch_i.txt
-python InsertMeasurements.py r2_12_47_2_node_v.txt
+#python InsertMeasurements.py r2_12_47_2_special.txt
+#python InsertMeasurements.py r2_12_47_2_switch_i.txt
+#python InsertMeasurements.py r2_12_47_2_node_v.txt
 #python InsertMeasurements.py r2_12_47_2_loads.txt
 #python InsertMeasurements.py r2_12_47_2_lines_pq.txt
 #python InsertMeasurements.py r2_12_47_2_xfmr_pq.txt
