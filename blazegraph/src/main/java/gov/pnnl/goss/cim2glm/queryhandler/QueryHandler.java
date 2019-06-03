68890ce8ecfae49616c1dba924323c48cdb88165
Tara Gibson <tara@pnnl.gov>
2019-06-03 14:50:19 -0700

    Added construct option to query handler since the result closable does not work on construct queries

--- blazegraph/src/main/java/gov/pnnl/goss/cim2glm/queryhandler/QueryHandler.java
+++ blazegraph/src/main/java/gov/pnnl/goss/cim2glm/queryhandler/QueryHandler.java
@@ -1,9 +1,17 @@
 package gov.pnnl.goss.cim2glm.queryhandler;
 
+import org.apache.jena.query.ResultSet;
 import org.apache.jena.query.ResultSetCloseable;
 
+import gov.pnnl.goss.cim2glm.components.DistComponent;
+
 public interface QueryHandler {
+	public final String Q_PREFIX = "PREFIX r: <" + DistComponent.nsRDF + "> PREFIX c: <" + DistComponent.nsCIM + "> PREFIX xsd:<" + DistComponent.nsXSD + "> ";
+
+	
+	
 	public ResultSetCloseable query(String szQuery);
+	public ResultSet construct(String szQuery);
 	public boolean addFeederSelection (String mRID); // TODO: support more than one, return False if not present
 	public boolean clearFeederSelections ();
 	public String getFeederSelection ();
--- blazegraph/src/main/java/gov/pnnl/goss/cim2glm/queryhandler/impl/HTTPBlazegraphQueryHandler.java
+++ blazegraph/src/main/java/gov/pnnl/goss/cim2glm/queryhandler/impl/HTTPBlazegraphQueryHandler.java
@@ -13,6 +13,8 @@ import gov.pnnl.goss.cim2glm.components.DistComponent;
 import gov.pnnl.goss.cim2glm.queryhandler.QueryHandler;
 
 public class HTTPBlazegraphQueryHandler implements QueryHandler {
+	
+	
 	String endpoint;
 	String mRID;
 	boolean use_mRID;
@@ -33,7 +35,6 @@ public class HTTPBlazegraphQueryHandler implements QueryHandler {
 
 	@Override
 	public ResultSetCloseable query(String szQuery) { 
-		String qPrefix = "PREFIX r: <" + DistComponent.nsRDF + "> PREFIX c: <" + DistComponent.nsCIM + "> PREFIX xsd:<" + DistComponent.nsXSD + "> ";
 		Query query;
 		if (use_mRID) { // try to insert a VALUES block for the feeder mRID of interest
 			String insertion_point = "WHERE {";
@@ -42,18 +43,44 @@ public class HTTPBlazegraphQueryHandler implements QueryHandler {
 //				System.out.println ("\n***");
 //				System.out.println (szQuery);
 //				System.out.println ("***");
-				StringBuilder buf = new StringBuilder (qPrefix + szQuery.substring (0, idx) + insertion_point + " VALUES ?fdrid {\"");
+				StringBuilder buf = new StringBuilder (Q_PREFIX + szQuery.substring (0, idx) + insertion_point + " VALUES ?fdrid {\"");
 				buf.append (mRID + "\"} " + szQuery.substring (idx + insertion_point.length()));
 //				System.out.println ("Sending " + buf.toString());
 				query = QueryFactory.create (buf.toString());
 			} else {
-				query = QueryFactory.create (qPrefix + szQuery);
+				query = QueryFactory.create (Q_PREFIX + szQuery);
 			}
 		} else {
-			query = QueryFactory.create (qPrefix + szQuery);
+			query = QueryFactory.create (Q_PREFIX + szQuery);
 		}
 		QueryExecution qexec = QueryExecutionFactory.sparqlService (endpoint, query);
 		ResultSetCloseable rs=  ResultSetCloseable.closeableResultSet(qexec);
+		return rs;
+	}
+	
+	@Override
+	public ResultSet construct(String szQuery) { 
+		Query query;
+		if (use_mRID) { // try to insert a VALUES block for the feeder mRID of interest
+			String insertion_point = "WHERE {";
+			int idx = szQuery.lastIndexOf (insertion_point);
+			if (idx >= 0) {
+//				System.out.println ("\n***");
+//				System.out.println (szQuery);
+//				System.out.println ("***");
+				StringBuilder buf = new StringBuilder (Q_PREFIX + szQuery.substring (0, idx) + insertion_point + " VALUES ?fdrid {\"");
+				buf.append (mRID + "\"} " + szQuery.substring (idx + insertion_point.length()));
+//				System.out.println ("Sending " + buf.toString());
+				query = QueryFactory.create (buf.toString());
+			} else {
+				query = QueryFactory.create (Q_PREFIX + szQuery);
+			}
+		} else {
+			query = QueryFactory.create (Q_PREFIX + szQuery);
+		}
+		QueryExecution qexec = QueryExecutionFactory.sparqlService(endpoint, query);
+//		qexec.
+		ResultSet rs=  qexec.execSelect();
 		return rs;
 	}
 	public boolean addFeederSelection (String mRID) {
