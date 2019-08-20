249b5613eba45192e1bb12aee2a2b83340b29dee
temcdrm <Thomas.McDermott@pnnl.gov>
2019-08-19 17:22:29 -0700

    working on CIM14 export

--- blazegraph/src/main/java/gov/pnnl/goss/cim2glm/CIMWriter.java
+++ blazegraph/src/main/java/gov/pnnl/goss/cim2glm/CIMWriter.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 import java.util.SortedSet;
 import java.util.TreeSet;
 import java.util.List;
+import java.util.UUID;
 
 import gov.pnnl.goss.cim2glm.CIMImporter;
 
@@ -56,23 +57,103 @@ import gov.pnnl.goss.cim2glm.components.DistXfmrCodeSCTest;
 import gov.pnnl.goss.cim2glm.components.DistXfmrTank;
 
 public class CIMWriter extends Object {
+	private static final String xnsCIM = "http://iec.ch/TC57/2009/CIM-schema-cim14#";
+	private static final String xnsRDF = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+
+	private String fdrID;
+
+	private void StartInstance (String root, String id, PrintWriter out) {
+		out.println (String.format("<cim:%s rdf:ID=\"%s\">", root, id));
+	}
+
+	private void StartFreeInstance (String root, PrintWriter out) {
+		String id = "_" + UUID.randomUUID().toString().toUpperCase();
+		StartInstance (root, id, out);
+	}
+
+	private void EndInstance (String root, PrintWriter out) {
+		out.println (String.format("</cim:%s>", root));
+	}
+
+	private void StringNode (String key, String val, PrintWriter out) {
+		out.println (String.format("  <cim:%s>%s</cim:%s>", key, val, key));
+	}
+
+	private void DoubleNode (String key, double val, PrintWriter out) {
+		out.println (String.format("  <cim:%s>%.8g</cim:%s>", key, val, key));
+	}
+
+	private void RefNode (String key, String val, PrintWriter out) {
+		out.println (String.format("  <cim:%s rdf:resource=\"#%s\"/>", key, val, key));
+	}
+
+	private void PhasesEnum (String val, PrintWriter out) {
+		out.println (String.format("  <cim:ConductingEquipment.phases rdf:resource=\"%sPhaseCode.%s\"/>", xnsCIM, val));
+	}
+
+	private void DistributionLineSegment (DistLineSegment obj, PrintWriter out) {
+		StartInstance ("DistributionLineSegment", obj.id, out);
+		StringNode ("IdentifiedObject.name", obj.name, out);
+		DoubleNode ("Conductor.length", obj.len, out);
+		RefNode ("Equipment.EquipmentContainer", fdrID, out);
+		PhasesEnum (obj.phases, out);
+		EndInstance ("DistributionLineSegment", out);  // needs GeoLocation and PSRType
+	}
 
 	public void WriteCIMFile (CIMImporter mdl, PrintWriter out)  {
-		mdl.PrintOneMap (mdl.mapFeeders, "*** FEEDERS ***");
-		out.println("{\"feeders\":[");
-		for (HashMap.Entry<String,DistFeeder> pair : mdl.mapFeeders.entrySet()) {
-			DistFeeder fdr = pair.getValue();
-			out.println("{\"name\":\"" + fdr.feederName + "\",");
-			out.println("\"mRID\":\"" + fdr.feederID + "\",");
-			out.println("\"substation\":\"" + fdr.substationName + "\",");
-			out.println("\"substationID\":\"" + fdr.substationID + "\",");
-			out.println("\"subregion\":\"" + fdr.subregionName + "\",");
-			out.println("\"subregionID\":\"" + fdr.subregionID + "\",");
-			out.println("\"region\":\"" + fdr.regionName + "\",");
-			out.println("\"regionID\":\"" + fdr.regionID + "\",");
+		out.println ("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+		out.println ("<!-- un-comment this line to enable validation");
+		out.println ("-->");
+		out.println ("<rdf:RDF xmlns:cim=\"" + xnsCIM + "\" xmlns:rdf=\"" + xnsRDF + "\">");
+		out.println ("<!--");
+		out.println ("-->");
+
+		StartFreeInstance ("IEC61970CIMVersion", out);
+		StringNode ("IEC61970CIMVersion.version", "IEC61970CIM14v12", out);
+		StringNode ("IEC61970CIMVersion.date", "2009-11-27T00:00:00", out);
+		EndInstance ("IEC61970CIMVersion", out);
+
+		for (HashMap.Entry<String,DistFeeder> fdrPair : mdl.mapFeeders.entrySet()) {
+			DistFeeder fdr = fdrPair.getValue();
+			fdrID = fdr.feederID;
+
+			StartInstance ("GeographicalRegion", fdr.regionID, out);
+			StringNode ("IdentifiedObject.name", fdr.regionName, out);
+			StringNode ("IdentifiedObject.description", "Top-level region", out);
+			EndInstance ("GeographicalRegion", out);
+
+			StartInstance ("SubGeographicalRegion", fdr.subregionID, out);
+			StringNode ("IdentifiedObject.name", fdr.subregionName, out);
+			StringNode ("IdentifiedObject.description", "Lower-level region", out);
+			RefNode ("SubGeographicalRegion.Region", fdr.regionID, out);
+			EndInstance ("SubGeographicalRegion", out);
+
+			StartInstance ("Substation", fdr.substationID, out);  // needs GeoLocation
+			StringNode ("IdentifiedObject.name", fdr.substationName, out);
+			EndInstance ("Substation", out);
+
+			StartInstance ("Line", fdrID, out);
+			StringNode ("IdentifiedObject.name", fdr.feederName, out);
+			StringNode ("IdentifiedObject.description", "Feeder equipment container", out);
+			RefNode ("Line.Region", fdr.subregionID, out);
+			EndInstance ("Line", out);
+
+			for (HashMap.Entry<String,DistLinesSpacingZ> pair : mdl.mapLinesSpacingZ.entrySet()) {
+				DistLinesSpacingZ obj = pair.getValue();
+				DistributionLineSegment (obj, out);
+			}
+			for (HashMap.Entry<String,DistLinesInstanceZ> pair : mdl.mapLinesInstanceZ.entrySet()) {
+				DistLinesInstanceZ obj = pair.getValue();
+				DistributionLineSegment (obj, out);
+			}
+			for (HashMap.Entry<String,DistLinesCodeZ> pair : mdl.mapLinesCodeZ.entrySet()) {
+				DistLinesCodeZ obj = pair.getValue();
+				DistributionLineSegment (obj, out);
+			}
 		}
-		out.println("]}");
-		out.close();
+
+		out.println ("</rdf:RDF>");
+		out.close ();
 	}
 }
 
