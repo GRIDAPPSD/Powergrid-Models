8c77f4d6828a42fb2b4a74da00d7e88f337b0747
temcdrm <Thomas.McDermott@pnnl.gov>
2019-11-08 15:02:28 -0800

    automating the generation of Transactive.xml

--- blazegraph/test/glm/pnnl/go.sh
+++ blazegraph/test/glm/pnnl/go.sh
@@ -2,6 +2,8 @@ python3 ../../../../taxonomy/converter_gld_dss.py transactive123.glm "4.16,0.48,
 python3 patch_xy.py
 cp BusCoords.new BusCoords.csv
 opendsscmd ConvertTransactive.ds
+cp Transactive.xml ../..
+cp transactive_*.csv ../..
 # cp BusCoords.csv ~/Documents/PNNL/ZZZ
 # cp *.dss ~/Documents/PNNL/ZZZ
 
