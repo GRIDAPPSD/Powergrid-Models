33032cf7fe21ed2d41d7c7e0e337b75b76728437
temcdrm <Thomas.McDermott@pnnl.gov>
2019-11-14 15:50:18 -0800

    changed name of ieee123transactive feeder in the source and CIM XML files; created the house and loading scripts for 9500-node speed profiling

--- blazegraph/test/glm/pnnl/go.bat
+++ blazegraph/test/glm/pnnl/go.bat
@@ -1,4 +1,4 @@
-python ..\..\..\..\taxonomy\converter_gld_dss.py transactive123.glm "4.16,0.48,0.208"
+python ..\..\..\..\taxonomy\converter_gld_dss.py transactive123.glm "4.16,0.48,0.208" ieee123transactive
 python patch_xy.py
 copy BusCoords.new BusCoords.csv
 opendsscmd ConvertTransactive.ds
