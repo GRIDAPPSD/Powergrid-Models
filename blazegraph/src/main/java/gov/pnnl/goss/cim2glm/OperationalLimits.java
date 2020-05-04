2329cfb78fbe56bc79010fc6789c5e5d0d7376f4
temcdrm <Thomas.McDermott@pnnl.gov>
2020-05-04 15:08:17 -0700

    adding bus name and coordinates to the voltage limit dictionary

--- blazegraph/src/main/java/gov/pnnl/goss/cim2glm/OperationalLimits.java
+++ blazegraph/src/main/java/gov/pnnl/goss/cim2glm/OperationalLimits.java
@@ -16,19 +16,41 @@ import gov.pnnl.goss.cim2glm.queryhandler.QueryHandler;
 import gov.pnnl.goss.cim2glm.queryhandler.impl.HTTPBlazegraphQueryHandler;
 
 public class OperationalLimits extends Object {
+  class VoltageLimit {
+    public String id;
+    public String bus;
+    public double x;
+    public double y;
+    public double Alo;
+    public double Ahi;
+    public double Blo;
+    public double Bhi;
+    public VoltageLimit (String id, String bus, double x, double y, double Blo, double Alo, double Ahi, double Bhi) {
+      this.id = id;
+      this.bus = bus;
+      this.x = x;
+      this.y = y;
+      this.Alo = Alo;
+      this.Ahi = Ahi;
+      this.Blo = Blo;
+      this.Bhi = Bhi;
+    }
+  }
+
 	QueryHandler queryHandler;
 	// an ascending list of voltage thresholds, indexed by ConnectivityNode ID
 	// normally the indexing will be 0:ANSI B Range Low, 1:A Low, 2:A High, 3: B High
-	HashMap<String,double[]> mapVoltageLimits = new HashMap<>();
+	HashMap<String,VoltageLimit> mapVoltageLimits = new HashMap<>();
 	// an ascending list of current magnitude thresholds, indexed by equipment ID
 	// normally the indexing will be 0:normal, 1:emergency
 	HashMap<String,double[]> mapCurrentLimits = new HashMap<>();
 
 	private static final String szVOLT = 
-		"SELECT ?id ?val ?dur ?dir WHERE {"+
+		"SELECT ?id ?val ?dur ?dir ?bus ?x ?y WHERE {"+
 		" ?fdr c:IdentifiedObject.mRID ?fdrid."+
 		" ?s c:ConnectivityNode.ConnectivityNodeContainer ?fdr."+
 		" ?s r:type c:ConnectivityNode."+
+    " ?s c:IdentifiedObject.name ?bus."+
 		" ?s c:IdentifiedObject.mRID ?id."+
 		" ?s c:ConnectivityNode.OperationalLimitSet ?ols."+
 		" ?vlim c:OperationalLimit.OperationalLimitSet ?ols."+
@@ -38,6 +60,14 @@ public class OperationalLimits extends Object {
 		" ?olt c:OperationalLimitType.direction ?rawdir."+
 		"  bind(strafter(str(?rawdir),\"OperationalLimitDirectionKind.\") as ?dir)"+
 		" ?vlim c:VoltageLimit.value ?val."+
+		" ?trm c:Terminal.ConnectivityNode ?s."+
+ 		" ?trm c:Terminal.ConductingEquipment ?eq."+
+		" ?eq c:PowerSystemResource.Location ?loc."+
+		" ?trm c:ACDCTerminal.sequenceNumber ?tseq."+
+		" ?pt c:PositionPoint.sequenceNumber ?tseq."+
+		" ?pt c:PositionPoint.Location ?loc."+
+		" ?pt c:PositionPoint.xPosition ?x."+
+		" ?pt c:PositionPoint.yPosition ?y."+
 		"} ORDER by ?id ?val";
 
 	private static final String szCURR = 
@@ -59,14 +89,14 @@ public class OperationalLimits extends Object {
 	private void LoadVoltageMap () {
 		ResultSet results = queryHandler.query (szVOLT, "voltage map");
 		String lastID = "";
-		double Alo = 0.0, Ahi = 1.0e9, Blo = 1.0e9, Bhi = 0.0;
+    String bus = "";
+		double Alo = 0.0, Ahi = 1.0e9, Blo = 1.0e9, Bhi = 0.0, x = 0.0, y = 0.0;
 		while (results.hasNext()) {
 			QuerySolution soln = results.next();
 			String id = soln.get("?id").toString();
 			if (!id.equals(lastID)) {
 				if (!lastID.equals("")) {
-					double [] vals = {Blo, Alo, Ahi, Bhi};
-					mapVoltageLimits.put(lastID, vals);
+					mapVoltageLimits.put(lastID, new VoltageLimit (lastID, bus, x, y, Blo, Alo, Ahi, Bhi));
 				}
 				Alo = 0.0;   // want the highest low value
 				Blo = 1.0e9; // want the lowest low value
@@ -75,6 +105,9 @@ public class OperationalLimits extends Object {
 				lastID = id;
 			}
 			String dir = soln.get("?dir").toString();
+      bus = soln.get("?bus").toString();
+      x = Double.parseDouble (soln.get("?x").toString());
+      y = Double.parseDouble (soln.get("?y").toString());
 			double dur = Double.parseDouble (soln.get("?dur").toString());
 			double val = Double.parseDouble (soln.get("?val").toString());
 			if (dir.equals("low")) {
@@ -95,27 +128,30 @@ public class OperationalLimits extends Object {
 		}
 		((ResultSetCloseable)results).close();
 		if (!mapVoltageLimits.containsKey (lastID)) {
-			double [] vals = {Blo, Alo, Ahi, Bhi};
-			mapVoltageLimits.put(lastID, vals);
+      mapVoltageLimits.put(lastID, new VoltageLimit (lastID, bus, x, y, Blo, Alo, Ahi, Bhi));
 		}
 	}
 
 	public void VoltageMapToJSON (PrintWriter out) {
 		DecimalFormat df2 = new DecimalFormat("#0.00");
+    DecimalFormat df4 = new DecimalFormat("#0.0000");
 		int idxLast = mapVoltageLimits.size() - 1;
 		int idx = 0;
 		String sTerm = "},";
-		for (HashMap.Entry<String,double[]> pair : mapVoltageLimits.entrySet()) {
-			double []vals = pair.getValue();
+		for (HashMap.Entry<String,VoltageLimit> pair : mapVoltageLimits.entrySet()) {
+			VoltageLimit vLim = pair.getValue();
 			String id = pair.getKey();
 			if (idx == idxLast) {
 				sTerm = "}";
 			}
 			out.println("{\"id\":\"" + id + "\"" +
-									 ",\"Blo\":" + df2.format (vals[0]) +
-									 ",\"Alo\":" + df2.format (vals[1]) +
-									 ",\"Ahi\":" + df2.format (vals[2]) +
-									 ",\"Bhi\":" + df2.format (vals[3]) + sTerm);
+									 ",\"Blo\":" + df2.format (vLim.Blo) +
+									 ",\"Alo\":" + df2.format (vLim.Alo) +
+									 ",\"Ahi\":" + df2.format (vLim.Ahi) +
+									 ",\"Bhi\":" + df2.format (vLim.Bhi) + 
+                   ",\"ConnectivityNode\":\"" + vLim.bus + "\"" +
+                   ",\"x\":" + df4.format (vLim.x) +
+                   ",\"y\":" + df4.format (vLim.y) + sTerm);
 			++idx;
 		}
 	}
