75bfcbdb1819a051fec6dfe45f30c1429943b982
temcdrm <Thomas.McDermott@pnnl.gov>
2020-05-18 13:46:08 -0700

    fix a case sensitivity for Ubuntu

--- blazegraph/test/glm/pnnl/go.sh
+++ blazegraph/test/glm/pnnl/go.sh
@@ -1,6 +1,6 @@
 python3 ../../../../taxonomy/converter_gld_dss.py transactive123.glm "4.16,0.48,0.208" ieee123transactive
 python3 patch_xy.py
-cp BusCoords.new BusCoords.csv
+cp BusCoords.new Buscoords.csv
 opendsscmd ConvertTransactive.ds
 cp Transactive.xml ../..
 cp transactive_*.csv ../..
