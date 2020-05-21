918c38539c422d28c5273390f2c5cfafca117d3b
temcdrm <thomas.mcdermott@pnnl.gov>
2020-05-21 15:05:59 -0700

    updating the converter for PNNL campus feeder

--- converters/convert_all.bat
+++ converters/convert_all.bat
@@ -5,7 +5,8 @@ rem python Cyme2DSS.py SampleCyme4.json
 
 rem python Cyme2DSS.py SampleCyme5.json
 rem python Cyme2DSS.py SampleCyme6.json
-python Cyme2DSS.py SampleCyme7.json
+rem python Cyme2DSS.py SampleCyme7.json
 
 rem python mdb2dss.py SampleSynergi1.json
+python mdb2dss.py SampleSynergi2.json
 
