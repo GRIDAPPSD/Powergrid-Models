94d71ec4cecb258b935386226ddf5e152cdcf6e1
temcdrm <Thomas.McDermott@pnnl.gov>
2021-02-08 10:04:23 -0800

    insert, drop and test DER working

--- platform/DER/test_der.sh
+++ platform/DER/test_der.sh
@@ -3,8 +3,10 @@ source envars.sh
 java -cp $CIMHUB_PATH $CIMHUB_PROG -s=_503D6E20-F499-4CC7-8051-971E23D0BF79 -o=both -l=1.0 -i=1 test/transactive_der
 
 cd test
-opendsscmd transactive_der_base.dss
+# rm *_s.csv
+opendsscmd check_der.dss
 gridlabd -D WANT_VI_DUMP=1 transactive_der_run.glm >transactive_der.log
 
+tail *_s.csv
 tail *.log
 
