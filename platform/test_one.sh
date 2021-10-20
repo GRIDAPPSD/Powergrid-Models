d57eaa971260c4f4a945a91991875fc9e9b3952e
temcdrm <Thomas.McDermott@pnnl.gov>
2021-10-20 09:20:50 -0700

    finished #1562, using SeriesCompensator for reactors

--- platform/test_one.sh
+++ platform/test_one.sh
@@ -6,7 +6,7 @@ source envars.sh
 # part of ./convert_xml.sh
 curl -D- -X POST $DB_URL --data-urlencode "update=drop all"
 curl -D- -H "Content-Type: application/xml" --upload-file /home/tom/src/Powergrid-Models/platform/cimxml/IEEE8500.xml -X POST $DB_URL
-java -cp $CIMHUB_PATH $CIMHUB_PROG -u=$DB_URL -o=both -l=1.0 -i=1 /home/tom/src/Powergrid-Models/platform/both/IEEE8500
+java -cp $CIMHUB_PATH $CIMHUB_PROG -u=$DB_URL -o=both -l=1.0 -i=1 -t=1 /home/tom/src/Powergrid-Models/platform/both/IEEE8500
 
 opendsscmd check_one.dss
 
