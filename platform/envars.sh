62cc33bffee888000fba2cb1119b86e9a71f1c1b
temcdrm <Thomas.McDermott@pnnl.gov>
2021-10-20 07:27:52 -0700

    working on #1562, scaling the GridLAB-D line_configuration to per-mile for DistLinesSinstanceZ

--- platform/envars.sh
+++ platform/envars.sh
@@ -2,6 +2,6 @@
 #declare -r DB_URL="http://blazegraph:8080/bigdata/namespace/kb/sparql"
 declare -r DB_URL="http://localhost:8889/bigdata/namespace/kb/sparql"
 declare -r SRC_PATH="/home/tom/src/Powergrid-Models/platform/"
-declare -r CIMHUB_PATH="../../CIMHub/target/libs/*:../../CIMHub/cimhub/target/cimhub-0.0.3-SNAPSHOT.jar"
+declare -r CIMHUB_PATH="../../CIMHub/cimhub/target/libs/*:../../CIMHub/cimhub/target/cimhub-0.0.3-SNAPSHOT.jar"
 declare -r CIMHUB_PROG="gov.pnnl.gridappsd.cimhub.CIMImporter"
 
