8f4482effa4716e9d66bedc0c43ccc23f6a7c787
temcdrm <Thomas.McDermott@pnnl.gov>
2022-04-05 17:49:19 -0400

    updating for transformer short-circuit tests and the final 9500-node balanced model, reusing mRIDs to the extent possible

--- platform/DER/envars.sh
+++ platform/DER/envars.sh
@@ -2,7 +2,5 @@
 #declare -r DB_URL="http://blazegraph:8080/bigdata/namespace/kb/sparql"
 declare -r DB_URL="http://localhost:8889/bigdata/namespace/kb/sparql"
 declare -r SRC_PATH="/home/tom/src/Powergrid-Models/platform/"
-declare -r CIMHUB_PATH="../../../CIMHub/cimnub/target/libs/*:../../../CIMHub/cimhub/target/cimhub-1.0.0-SNAPSHOT.jar"
+declare -r CIMHUB_PATH="../../../CIMHub/cimnub/target/libs/*:../../../CIMHub/cimhub/target/cimhub-1.0.1-SNAPSHOT.jar"
 declare -r CIMHUB_PROG="gov.pnnl.gridappsd.cimhub.CIMImporter"
-
-
--- platform/load_all_cimxml.sh
+++ platform/load_all_cimxml.sh
@@ -11,7 +11,7 @@ curl -D- -H "Content-Type: application/xml" --upload-file ./cimxml/IEEE13.xml -X
 curl -D- -H "Content-Type: application/xml" --upload-file ./cimxml/IEEE13_Assets.xml -X POST $DB_URL
 curl -D- -H "Content-Type: application/xml" --upload-file ./cimxml/IEEE37.xml -X POST $DB_URL
 curl -D- -H "Content-Type: application/xml" --upload-file ./cimxml/IEEE8500.xml -X POST $DB_URL
-curl -D- -H "Content-Type: application/xml" --upload-file ./cimxml/IEEE8500_3subs.xml -X POST $DB_URL
+#curl -D- -H "Content-Type: application/xml" --upload-file ./cimxml/IEEE8500_3subs.xml -X POST $DB_URL
 curl -D- -H "Content-Type: application/xml" --upload-file ./cimxml/R2_12_47_2.xml -X POST $DB_URL
 curl -D- -H "Content-Type: application/xml" --upload-file ./cimxml/Transactive.xml -X POST $DB_URL
 #curl -D- -H "Content-Type: application/xml" --upload-file ./cimxml/final9500nodebalanced.xml -X POST $DB_URL
